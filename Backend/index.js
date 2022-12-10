47365d8c47503d27ceb10cf3cc2e1eb66db1619e
Rohin Joshi <rohinjoshi@Rohins-MacBook-Air.local>
2022-12-10 22:34:15 +0530

    Adding backend

---
+++ Backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const router = express.Router();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
+
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
+
---
+++ Backend/routes/conversations.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const Conversation = require("../models/Conversation");
+
+//new conversation
+
+router.post("/",async(req,res)=>{
+    const newConversation = new Conversation({
+        members: [req.body.senderId,req.body.receiverId],
+    });
+    try {
+        const savedConversation = await newConversation.save();
+        res.status(200).json(savedConversation);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+//Get conversation
+
+router.get("/:userId",async(req,res)=>{
+    try {
+        const conversation = await Conversation.find({
+            members:{ $in:[req.params.userId]},
+        });
+        res.status(200).json(conversation);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//get conv between 2 peeps
+router.get("/find/:firstUserId/:SecondUserId",async (req,res)=>{
+    try{const conversation = await Conversation.findOne({
+        members:{$all: [req.params.firstUserId,req.params.SecondUserId]},
+    });
+    res.status(200).json(conversation);
+}
+    catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
